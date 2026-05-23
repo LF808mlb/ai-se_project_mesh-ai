1a820fc4caf6b695a0b1830bf72fb2974bfd8cad
lesley <sprite_lksln31@yahoo.com>
2026-05-23 07:22:51 -1000

    git commit -m "add middleware"

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 
 import express from "express";
 import router from "./routes/index.js";
+import { logger } from "./middleware/logger.js";
 const app = express();
 
 
 const port = 3000;
 
+
+
+app.use(express.json());
+app.use(logger);
 
 app.get("/health", (req, res): void => {
   res.status(200).json({
