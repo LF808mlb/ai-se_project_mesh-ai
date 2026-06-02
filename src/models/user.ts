8af5a58507b31dae2103caae52cda0acb53aec8f
lesley <sprite_lksln31@yahoo.com>
2026-06-02 10:02:03 -1000

    git commit -m "create user.ts model"

---
+++ src/models/user.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+export interface IUser {
+	email: string;
+	password: string;
+	name: string;
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+	email: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	name: {
+		type: String,
+		required: true
+	}
+});
+
+const User = mongoose.models.User || mongoose.model<IUser>("User", userSchema);
+
+export default User;
