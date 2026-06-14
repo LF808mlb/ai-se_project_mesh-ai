094150db5b33b807316fc583661dcbf574aa7b8e
lesley <sprite_lksln31@yahoo.com>
2026-06-13 15:10:44 -1000

    git commit -m "1st part of Step 5: Build the page skeleton"

--- client/src/pages/KnowledgeBase/KnowledgeBase.tsx
+++ client/src/pages/KnowledgeBase/KnowledgeBase.tsx
@@ -1,5 +1,12 @@
+import { useState } from "react";
 import "./KnowledgeBase.css";
+import type { KnowledgeDoc } from "../../utils/api";
 
 export default function KnowledgeBase() {
+
+  const [documents, setDocuments] = useState<KnowledgeDoc[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
   return <div>Knowledge Base</div>;
 }
