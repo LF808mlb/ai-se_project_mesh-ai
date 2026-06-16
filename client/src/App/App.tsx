ec9bba2f6537aa1078e4ae6894a19a4956c6773f
lesley <sprite_lksln31@yahoo.com>
2026-06-16 09:57:56 -1000

    git commit -m "routes and navigation step 1"

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,11 +1,23 @@
-import "./App.css"
+import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AppLayout from "../components/AppLayout/AppLayout";
+import Chat from "../pages/Chat/Chat";
+import Intro from "../pages/Intro/Intro";
 import KnowledgeBase from "../pages/KnowledgeBase/KnowledgeBase";
-
 
 function App() {
   return (
     <div className="app">
-      <KnowledgeBase />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate to="/knowledge-base" replace />} />
+          <Route path="intro" element={<Intro />} />
+          <Route element={<AppLayout />}>
+            <Route path="knowledge-base" element={<KnowledgeBase />} />
+            <Route path="chat" element={<Chat />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
