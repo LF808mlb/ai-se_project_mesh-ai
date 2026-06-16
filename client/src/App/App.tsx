20e74c03091014d2e5c13529a3169b985ef901a9
lesley <sprite_lksln31@yahoo.com>
2026-06-16 10:38:56 -1000

    git commit -m "step 2 set up routes"

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import AppLayout from "../components/AppLayout/AppLayout";
 import Chat from "../pages/Chat/Chat";
 import Intro from "../pages/Intro/Intro";
@@ -8,16 +8,13 @@ import KnowledgeBase from "../pages/KnowledgeBase/KnowledgeBase";
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Navigate to="/knowledge-base" replace />} />
-          <Route path="intro" element={<Intro />} />
-          <Route element={<AppLayout />}>
-            <Route path="knowledge-base" element={<KnowledgeBase />} />
-            <Route path="chat" element={<Chat />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Intro />} />
+        <Route element={<AppLayout />}>
+          <Route path="/knowledge" element={<KnowledgeBase />} />
+          <Route path="/chat" element={<Chat />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
