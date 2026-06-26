f98d5b1a539390c870ad8ebfc7c9405e251489ed
lesley <sprite_lksln31@yahoo.com>
2026-06-26 10:40:28 -1000

    git commit -m "login and register form Step 1: Add the routes"

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -4,12 +4,16 @@ import AppLayout from "../AppLayout/AppLayout";
 import Chat from "../../pages/Chat/Chat";
 import Intro from "../../pages/Intro/Intro";
 import KnowledgeBase from "../../pages/KnowledgeBase/KnowledgeBase";
+import Login from "../../pages/Login/Login";
+import Register from "../../pages/Register/Register";
 
 function App() {
   return (
     <div className="app">
       <Routes>
         <Route path="/" element={<Intro />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route element={<AppLayout />}>
           <Route path="/knowledge" element={<KnowledgeBase />} />
           <Route path="/chat" element={<Chat />} />
---
+++ client/src/pages/Register/Register.tsx
@@ -0,0 +1,14 @@
+import logo from "../../assets/logo.png";
+
+export default function Register() {
+	return (
+		<main>
+			<header className="header">
+				<img className="header__logo" alt="MeshAI logo" src={logo} />
+			</header>
+			<form>
+				<h1>Register</h1>
+			</form>
+		</main>
+	);
+}
